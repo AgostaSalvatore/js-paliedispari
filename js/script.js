b07f8246cb31b670788799d73681a6416a7edee9
AgostaSalvatore <agosta.boolean@gmail.com>
2025-03-21 13:31:37 +0100

    Refactor palindroma function to reverse input string and log result

--- js/script.js
+++ js/script.js
@@ -2,10 +2,14 @@ console.log('----------Palindroma----------');
 
 // Chiedere all'utente di inserire una parola
 let world = prompt('Inserisci una parola');
+//inizializzo la variabile di conforto per capire se la parola è palindroma
+let worldReverse ='';
 //iniziallizzo la funzione per capire se la parola è palindroma
 function palindroma(world){
-    
+    worldReverse = world.split('').reverse().join('');
 }
+
+console.log(palindroma(world));
 
 
 
