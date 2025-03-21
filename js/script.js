fb04311f02b79fc8405e820bafb53ff6d97b7f37
AgostaSalvatore <agosta.boolean@gmail.com>
2025-03-21 13:26:19 +0100

    Update  script.js for Palindroma base

---
+++ js/script.js
@@ -0,0 +1,16 @@
+console.log('----------Palindroma----------');
+
+// Chiedere all'utente di inserire una parola
+let world = prompt('Inserisci una parola');
+//iniziallizzo la funzione per capire se la parola è palindroma
+function palindroma(world){
+    
+}
+
+
+
+
+
+
+
+console.log('----------Pari-Dispari----------');
